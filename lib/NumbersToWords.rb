d5bf35f4c03efe3a6f97705db9683da4fe34cde7
muhanad <muhanad40@gmail.com>
2014-05-01 12:02:42 +0100

    Added method that converts a segment of 3 (max) digits into words and tested it

--- lib/NumbersToWords.rb
+++ lib/NumbersToWords.rb
@@ -1,5 +1,41 @@
 class NumbersToWords
+
+	WORDS = {
+		ones: 'one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen'.split(" ").map(&:capitalize),
+		tens: 'ten twenty thirty fourty fifty sixty seventy eighty ninty'.split(" ").map(&:capitalize),
+	}
+
 	def format(number)
 		number.to_s.chars.reverse.each_slice(3).map(&:join).join(",").reverse
 	end
+
+	def convert_segment(number)
+		output = ''
+		number_int = number.to_i
+		number_str = number.to_s
+		length = number_str.length
+		return 'zero' if number_int == 0
+		return false if length > 3
+		if ( length == 1 || length == 2 ) && number_int < 20
+			output += WORDS[:ones][number_int-1]
+		elsif length == 2 && number_int >= 20
+			segment_1 = number_str[0].to_i
+			segment_2 = number_str[1].to_i
+			output += WORDS[:tens][segment_1.to_i-1]
+			output += " " + WORDS[:ones][segment_2-1] if segment_2 > 0
+		elsif length == 3 && number_int >= 20
+			segment_1 = number_str[0].to_i
+			segment_2 = (number_str[1]+number_str[2]).to_i
+			output += WORDS[:ones][segment_1.to_i-1]
+			output += " Hundred and "
+			if segment_2 < 20
+				output += WORDS[:ones][segment_2-1]
+			elsif segment_2 >= 20
+				output += WORDS[:tens][number_str[1].to_i-1]
+				output += " " + WORDS[:ones][number_str[2].to_i-1] if number_str[2].to_i > 0
+			end
+		end
+		output
+	end
+
 end
--- spec/NumbersToWords_spec.rb
+++ spec/NumbersToWords_spec.rb
@@ -8,4 +8,20 @@ describe NumbersToWords do
 		expect(number_to_words.format(1234)).to eq "1,234"
 	end
 
+	context 'when given a segment array' do
+
+		it 'converts the "1" to "One"' do
+			expect(number_to_words.convert_segment(1)).to eq "One"
+		end
+
+		it 'converts the "15" to "Fifteen"' do
+			expect(number_to_words.convert_segment(15)).to eq "Fifteen"
+		end
+
+		it 'converts the "371" to "Three Hundred and Seventy One"' do
+			expect(number_to_words.convert_segment(371)).to eq "Three Hundred and Seventy One"
+		end
+
+	end
+
 end
