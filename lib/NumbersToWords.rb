40288ac52f485337190fc18685685ba8ae9dd56c
muhanad <muhanad40@gmail.com>
2014-05-01 11:57:00 +0100

    Renamed the class

--- lib/NumbersToWords.rb
+++ lib/NumbersToWords.rb
@@ -1,4 +1,4 @@
-class NumberToWords
+class NumbersToWords
 	def format(number)
 		number.to_s.chars.reverse.each_slice(3).map(&:join).join(",").reverse
 	end
--- spec/NumbersToWords_spec.rb
+++ spec/NumbersToWords_spec.rb
@@ -1,8 +1,8 @@
-require 'NumberToWords'
+require 'NumbersToWords'
 
-describe NumberToWords do
+describe NumbersToWords do
 
-	let(:number_to_words) { NumberToWords.new }
+	let(:number_to_words) { NumbersToWords.new }
 
 	it 'formats the number "1234" into "1,234"' do
 		expect(number_to_words.format(1234)).to eq "1,234"
