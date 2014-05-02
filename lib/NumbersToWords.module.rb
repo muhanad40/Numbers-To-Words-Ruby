1086d394a7fead9867759e72bdc2dd62a8a8840d
muhanad <muhanad40@gmail.com>
2014-05-02 15:48:41 +0100

    Rewrote the whole thing (refactored the crap out of it), handles numbers larger than 999,999,999

--- lib/NumbersToWords.module.rb
+++ lib/NumbersToWords.module.rb
@@ -1,57 +1,68 @@
 module NumbersToWords_Module
 
-	WORDS = {
-		ones: 'one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen'.split(" ").map(&:capitalize),
-		tens: 'ten twenty thirty fourty fifty sixty seventy eighty ninty'.split(" ").map(&:capitalize),
-	}
+	# ADD NAMES OF LARGE NUMBERS
+	WORDS = 'hundred thousand million billion trillion gazillion'.split(" ").map(&:capitalize)
 
-	def format(number)
-		number.to_s.chars.reverse.each_slice(3).map(&:join).join(",").reverse
+	# DO NOT EDIT THE FOLLOWING VALUES
+	ONES = 'one two three four five six seven eight nine ten'.split(" ").map(&:capitalize)
+	TENS = 'eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty'.split(" ").map(&:capitalize)
+	MULTIPLES_OF_TEN = 'ten twenty thirty fourty fifty sixty seventy eighty ninty hundred'.split(" ").map(&:capitalize)
+
+	def convert_number(number)
+		return false if number == 0
+		number = number.to_i
+		number_str = number.to_s
+		return false if number > 99
+		if number <= 10
+			result = ONES[number-1]
+		elsif number >= 11 && number <= 20
+			result = TENS[(number-1)-10]
+		elsif number >= 21 && number <= 99
+			result = MULTIPLES_OF_TEN[number_str[0].to_i-1]
+			result += ' ' + ONES[number_str[1].to_i-1]
+		end
+		result.to_s
 	end
 
 	def convert_segment(number)
-		output = ''
-		number_int = number.to_i
-		number_str = number.to_s
-		length = number_str.length
-		return 'zero' if number_int == 0
+		number_arr = number.to_s.split("")
+		length = number_arr.length
 		return false if length > 3
-		if ( length == 1 || length == 2 ) && number_int < 20
-			output += WORDS[:ones][number_int-1]
-		elsif length == 2 && number_int >= 20
-			segment_1 = number_str[0].to_i
-			segment_2 = number_str[1].to_i
-			output += WORDS[:tens][segment_1.to_i-1]
-			output += " " + WORDS[:ones][segment_2-1] if segment_2 > 0
-		elsif length == 3 && number_int >= 20
-			segment_1 = number_str[0].to_i
-			segment_2 = (number_str[1]+number_str[2]).to_i
-			output += WORDS[:ones][segment_1.to_i-1]
-			output += " Hundred and "
-			if segment_2 < 20
-				output += WORDS[:ones][segment_2-1]
-			elsif segment_2 >= 20
-				output += WORDS[:tens][number_str[1].to_i-1]
-				output += " " + WORDS[:ones][number_str[2].to_i-1] if number_str[2].to_i > 0
+		output = ''
+		if length == 3
+			output += convert_number(number.to_s[0])
+			output += ' ' + WORDS[0] + ' '
+			if number.to_s[1..-1].to_i > 0
+				output += 'and '
+				output += convert_number(number.to_s[1..-1])
 			end
+		else
+			output += convert_number(number.to_s[0..-1])
 		end
 		output
 	end
 
 	def convert(number)
-		number_arr = format(number).split(",")
-		total_segments = number_arr.count
-		output = ''
-		number_arr.each_with_index do |segment, count|
-			count = count+1
-			output += convert_segment(segment)
-			if (total_segments == 2 && count == 1) || (total_segments == 3 && count == 2)
-				output += " Thousand "
-			elsif total_segments == 3 && count == 1
-				output += " Million "
+		output = []
+		number_parsed = parse(number)
+		number_parsed.reverse.each_with_index do |segment, index|
+			if index == 0
+				output << convert_segment(segment.join)
+			else
+				output << convert_segment(segment.join) + ' ' + WORDS[index]
 			end
 		end
-		output
+		output.reverse.join(" ")
 	end
 
+	private
+
+	def parse(number)
+		number.to_s.split('').reverse.each_slice(3).to_a.map(&:reverse).reverse
+	end
+
+end
+
+class Converter
+	include NumbersToWords_Module
 end
