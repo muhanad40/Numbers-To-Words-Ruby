e85e596030a2fed0435435951de1968152f460ea
muhanad <muhanad40@gmail.com>
2014-05-06 09:45:27 +0100

    Corrected the tests

--- spec/NumbersToWords_spec.rb
+++ spec/NumbersToWords_spec.rb
@@ -10,33 +10,33 @@ describe NumbersToWords do
 	context 'when given a segment' do
 
 		it 'converts the "1" to "One"' do
-			expect(number_to_words.convert_segment(1)).to eq "One"
+			expect(number_to_words.send(:convert_segment, 1)).to eq "One"
 		end
 
 		it 'converts the "311" to "Three Hundred and Eleven"' do
-			expect(number_to_words.convert_segment(311)).to eq "Three Hundred and Eleven"
+			expect(number_to_words.send(:convert_segment, 311)).to eq "Three Hundred and Eleven"
 		end
 
 		it 'converts the "15" to "Fifteen"' do
-			expect(number_to_words.convert_segment(15)).to eq "Fifteen"
+			expect(number_to_words.send(:convert_segment, 15)).to eq "Fifteen"
 		end
 
-		it 'converts the "371" to "Three Hundred and Seventy One"' do
-			expect(number_to_words.convert_segment(371)).to eq "Three Hundred and Seventy One"
+		it 'converts the "371" to "Three Hundred and Seventy-One"' do
+			expect(number_to_words.send(:convert_segment, 371)).to eq "Three Hundred and Seventy-One"
 		end
 
 	end
 
-	it 'converts "1513243" to "One Million Five Hundred and Thirteen Thousand Two Hundred and Fourty Three' do
-		expect(number_to_words.convert(1513243)).to eq "One Million Five Hundred and Thirteen Thousand Two Hundred and Fourty Three"
+	it 'converts "1513243" to "One Million, Five Hundred and Thirteen Thousand, Two Hundred and Fourty-Three' do
+		expect(number_to_words.convert(1513243)).to eq "One Million, Five Hundred and Thirteen Thousand, Two Hundred and Fourty-Three"
 	end
 
-	it 'converts the string "123456" to "One Hundred and Twenty Three Thousand Four Hundred and Fifty Six"' do
-		expect("123456".NumbersToWords).to eq "One Hundred and Twenty Three Thousand Four Hundred and Fifty Six"
+	it 'converts the string "123456" to "One Hundred and Twenty-Three Thousand, Four Hundred and Fifty-Six"' do
+		expect("123456".NumbersToWords).to eq "One Hundred and Twenty-Three Thousand, Four Hundred and Fifty-Six"
 	end
 
-	it 'converts the integer 123456 to "One Hundred and Twenty Three Thousand Four Hundred and Fifty Six"' do
-		expect(123456.NumbersToWords).to eq "One Hundred and Twenty Three Thousand Four Hundred and Fifty Six"
+	it 'converts the integer 123456 to "One Hundred and Twenty-Three Thousand, Four Hundred and Fifty-Six"' do
+		expect(123456.NumbersToWords).to eq "One Hundred and Twenty-Three Thousand, Four Hundred and Fifty-Six"
 	end
 
 end
